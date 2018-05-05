d0da78d5054fd181df64db9074192d34f096e546
lexprom <lexprom009@gmail.com>
2018-05-05 13:44:24 +0400

    fixed ring and circle

--- Task2_Ring/Circle.cs
+++ Task2_Ring/Circle.cs
@@ -1,4 +1,9 @@
-﻿namespace ADO.NET_Task_2_b
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ring
 {
     public class Circle
     {
@@ -6,16 +11,17 @@
         public Circle(Point center, double radius)
         {
             Center = center;
-            R = radius;
+            Radius = radius;
         }
 
-        public double R
+        public double Radius
         {
             get => radius;
             set
             {
                 if (value <= 0)
                     throw new System.Exception("Radius can't be less than zero.");
+                radius = value;
             }
         }
         public Point Center { get; set; }
--- Task2_Ring/Ring.cs
+++ Task2_Ring/Ring.cs
@@ -1,46 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace ADO.NET_Task_2_b
+namespace Ring
 {
-    public class Ring
+   public class Ring
     {
         private double ringRadius;
         private Circle greaterCircle;
+
         public Ring(Circle greaterCircle, double ringRadius)
         {
             GreaterCircle = greaterCircle;
             RingRadius = ringRadius;
-        }
-
-        public double GetArea()
-        {
-            return Math.PI * (GreaterCircle.R * GreaterCircle.R - RingRadius * RingRadius);
-        }
-
-        public double GetGreaterCircleLength()
-        {
-            return 2 * Math.PI * GreaterCircle.R;
-        }
-
-        public double GetLesserCircleLength()
-        {
-            return 2 * Math.PI * RingRadius;
-        }
-
-        public void ChangeCircleAndRingRadius(Circle greaterCircle, double ringRadius)
-        {
-            if (greaterCircle == null || (greaterCircle != null && greaterCircle.R < ringRadius) || ringRadius < 0)
-                throw new Exception("Invalid arguments");
-
-            this.greaterCircle = greaterCircle;
-            this.ringRadius = ringRadius;
-        }
-
-        public override string ToString()
-        {
-            return $"Center - ({GreaterCircle.Center.X}, {GreaterCircle.Center.Y}), greater radius - {GreaterCircle.R}, lesser radius - {RingRadius}";
         }
 
         public Circle GreaterCircle
@@ -48,7 +18,7 @@ namespace ADO.NET_Task_2_b
             get => greaterCircle;
             set
             {
-                if (value == null && value.R > RingRadius)
+                if (value == null && value.Radius > RingRadius)
                     throw new Exception("Wrong circle!");
                 else
                     greaterCircle = value;
@@ -56,14 +26,34 @@ namespace ADO.NET_Task_2_b
         }
         public double RingRadius
         {
-            get => RingRadius;
+            get => ringRadius;
             set
             {
-                if (value < 0 || (GreaterCircle != null && value > GreaterCircle.R) || GreaterCircle == null)
+                if (value < 0 || value > GreaterCircle.Radius || GreaterCircle == null)
                     throw new Exception("Wrong radius!");
 
                 ringRadius = value;
             }
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * (GreaterCircle.Radius * GreaterCircle.Radius - RingRadius * RingRadius);
+        }
+
+        public double GetGreaterCircleLength()
+        {
+            return 2 * Math.PI * GreaterCircle.Radius;
+        }
+
+        public double GetLesserCircleLength()
+        {
+            return 2 * Math.PI * RingRadius;
+        }
+
+        public override string ToString()
+        {
+            return $"Center - ({GreaterCircle.Center.X}, {GreaterCircle.Center.Y}), greater radius - {GreaterCircle.Radius}, lesser radius - {RingRadius}";
         }
     }
 }
